62f3a5556628549178f7e9cfa4717b5adf3761be
Afrijal Dzuhri <ijuldz@gmail.com>
2019-10-22 21:07:16 +0700

    credential on change

--- src/components/Credential.js
+++ src/components/Credential.js
@@ -11,8 +11,23 @@ class Credential extends React.Component {
     age: '',
     occupation: '',
     province: '',
+    number: 1,
+  }
+
+  handleChange = e => {
+    console.log(e.target.value)
+  }
+
+  handleBack = e => {
+    e.preventDefault()
+    this.setState({ number: this.state.number !== 0 ? this.state.number - 1 : this.state.number })
   }
   
+  handleNext = e => {
+    e.preventDefault()
+    this.setState({ number: this.state.number !== 9 ? this.state.number + 1 : this.state.number })
+  }
+
   render() {
     return (
       <>
@@ -20,59 +35,59 @@ class Credential extends React.Component {
         <div className="container mt-4">
           <div className="card">
             <div className="card-body">
-              <form>
-                <div className="form-group">
+              <form onSubmit={this.handleNext}>
+                {this.state.number === 1 && <div className="form-group">
                   <label>Silakan isi dengan email aktif yang kamu gunakan</label>
-                  <input type="email" className="form-control" aria-describedby="emailHelp" placeholder="Enter email" />
-                </div>
+                  <input onChange={this.handleChange} type="email" className="form-control" placeholder="Enter email" />
+                </div>}
 
-                <div className="form-group">
+                {this.state.number === 2 && <div className="form-group">
                   <label>Dengan mengisi data diri serta menjawab pertanyaan di bawah ini, saya memahami bahwa data yang saya berikan akan dijaga kerahasiaannya.</label>
                   <div className="d-flex justify-content-between">
                     <div className="form-check">
-                      <input className="form-check-input" type="radio" name="exampleRadios" value="option1" checked />
+                      <input onChange={this.handleChange} className="form-check-input" type="radio" name="exampleRadios" value="option1" checked />
                         <label className="form-check-label">
                           Setuju
                         </label>
                       </div>
                       <div className="form-check">
-                        <input className="form-check-input" type="radio" name="exampleRadios" value="option2" />
+                        <input onChange={this.handleChange} className="form-check-input" type="radio" name="exampleRadios" value="option2" />
                           <label className="form-check-label">
                             Tidak setuju
                         </label>
                       </div>
                   </div>
-                </div>
+                </div>}
 
-                <div className="form-group">
+                {this.state.number === 3 && <div className="form-group">
                   <label>Sebelum itu, silakan isi data diri kamu ya!</label>
-                  <input type="text" className="form-control" aria-describedby="emailHelp" placeholder="Nama panggilan" />
-                </div>
+                  <input onChange={this.handleChange} type="text" className="form-control" aria-describedby="emailHelp" placeholder="Nama panggilan" />
+                </div>}
 
-                <div className="form-group">
+                {this.state.number === 4 && <div className="form-group">
                   <label>Jenis Kelamin</label>
                   <div className="d-flex justify-content-between">
                     <div className="form-check">
-                      <input className="form-check-input" type="radio" name="exampleRadios" value="option1" checked />
+                      <input onChange={this.handleChange} className="form-check-input" type="radio" name="exampleRadios" value="option1" checked />
                       <label className="form-check-label">
                         Laki-laki
                         </label>
                     </div>
                     <div className="form-check">
-                      <input className="form-check-input" type="radio" name="exampleRadios" value="option2" />
+                      <input onChange={this.handleChange} className="form-check-input" type="radio" name="exampleRadios" value="option2" />
                       <label className="form-check-label">
                         Perempuan
                       </label>
                     </div>
                   </div>
-                </div>
+                </div>}
 
-                <div className="form-group">
+                {this.state.number === 5 && <div className="form-group">
                   <label>Hanya isi dengan angka</label>
-                  <input type="number" className="form-control" aria-describedby="emailHelp" placeholder="Usia" />
-                </div>
+                  <input onChange={this.handleChange} type="number" className="form-control" aria-describedby="emailHelp" placeholder="Usia" />
+                </div>}
 
-                <div className="form-group">
+                {this.state.number === 6 && <div className="form-group">
                   <label>Pekerjaan</label>
                   <select className="form-control">
                     <option>1</option>
@@ -81,9 +96,9 @@ class Credential extends React.Component {
                     <option>4</option>
                     <option>5</option>
                   </select>
-                </div>
+                </div>}
 
-                <div className="form-group">
+                {this.state.number === 8 && <div className="form-group">
                   <label>Pendidikan Terakhir</label>
                   <select className="form-control">
                     <option>1</option>
@@ -92,15 +107,15 @@ class Credential extends React.Component {
                     <option>4</option>
                     <option>5</option>
                   </select>
-                </div>
+                </div>}
 
-                <div className="form-group">
+                {this.state.number === 9 && <div className="form-group">
                   <label>Provinsi saat ini</label>
-                  <input type="number" className="form-control" aria-describedby="emailHelp" placeholder="Usia" />
-                </div>
+                  <input onChange={this.handleChange} type="number" className="form-control" aria-describedby="emailHelp" placeholder="Usia" />
+                </div>}
                 
                 <div className="d-flex justify-content-between">
-                  <button type="submit" className="btn btn-default">Kembali</button>
+                  <button onClick={this.handleBack} className="btn btn-default">Kembali</button>
                   <button type="submit" className="btn btn-primary">Lanjutkan</button>
                 </div>
               </form>
